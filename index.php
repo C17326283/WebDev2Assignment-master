5642f3e6bca2740504d9896380d4e91a2cc43c06
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2019-11-18 13:39:27 +0000

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         <div class = "row top">
             <div class="col-sm-4 banner-box">
                 <a href="index.php">
-                <img class ="topLogo" src="res/logosample.png">
+                    <img class ="topLogo" src="res/logosample.png">
                 </a>
             </div>
             <div class="col-sm-8 banner-box">
@@ -35,48 +35,48 @@
         <nav>
             <div class = "row navRow">
                 <a href="index.php">
-                <div class="col-sm-2 tab-box">
-                    <img src="res/HomeWhite.png">
-                    <h6>Home</h6>
-                </div>
+                    <div class="col-sm-2 tab-box">
+                        <img src="res/HomeWhite.png">
+                        <h6>Home</h6>
+                    </div>
                 </a>
                 
                 <a href="Results.php">
-                <div class="col-sm-2 tab-box">
-                    <img src="res/magnifyingGlassIcon.png">
-                    <h6>Search</h6>
-                </div>
+                    <div class="col-sm-2 tab-box">
+                        <img src="res/magnifyingGlassIcon.png">
+                        <h6>Search</h6>
+                    </div>
                 </a>
                 <a href="https://www.google.com/">
-                <div class="col-sm-2 tab-box">
-                        <img src="res/questionIcon.png">
-                        <h6>How to use</h6>
-                </div>
-                    </a>
+                    <div class="col-sm-2 tab-box">
+                            <img src="res/questionIcon.png">
+                            <h6>How to use</h6>
+                    </div>
+                </a>
                 <a href="https://www.google.com/">
-                <div class="col-sm-2 tab-box">
-                    <img src="res/aboutUsIcon.png"><h6>About Us</h6>
-                </div>
-                    </a> 
+                    <div class="col-sm-2 tab-box">
+                        <img src="res/aboutUsIcon.png"><h6>About Us</h6>
+                    </div>
+                </a>
                 <a href="https://www.google.com/">
-                <div class="col-sm-2 tab-box">
-                    <img src="res/ContactIcon.png"><h6>Contact</h6>
-                </div>
-                    </a>
+                    <div class="col-sm-2 tab-box">
+                        <img src="res/ContactIcon.png"><h6>Contact</h6>
+                    </div>
+                </a>
                 <div class="col-sm-1 tab-box">
                 </div>
                     
                 <a href="https://www.google.com/">
-                <div class="col-sm-1 tab-box drop-box">
-                    <div class="dropdown">
-                        <img class="login" src="res/DropIcon.png" height="20px;">
-                        <div class="dropdown-menu">cd
-                            <a href="https://www.google.com/">Login</a>
-                            <a href="registerUser.php">Register</a>
-                            <a href="https://www.google.com/">test3</a>
+                    <div class="col-sm-1 tab-box drop-box">
+                        <div class="dropdown">
+                            <img class="login" src="res/DropIcon.png" height="20px;">
+                            <div class="dropdown-menu">cd
+                                <a href="https://www.google.com/">Login</a>
+                                <a href="registerUser.php">Register</a>
+                                <a href="https://www.google.com/">test3</a>
+                            </div>
                         </div>
                     </div>
-                </div>
                 </a>
             </div>
         </nav>
