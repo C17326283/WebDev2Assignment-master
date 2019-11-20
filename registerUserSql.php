106b3be473f75d3ef98c296b83b6e253507a5fa6
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2019-11-20 20:46:17 +0000

    userCheck

--- registerUserSql.php
+++ registerUserSql.php
@@ -15,6 +15,18 @@
                 die("Connection failed: " . $conn->connect_error);
             } 
             echo "Connected successfully <br>";
+    
+            //check if username is already taken
+            $checkUsername = $_POST["Username"];
+            $check = "SELECT * FROM users WHERE Username = '$checkUsername'"; 
+            $result = mysqli_query($conn, $check) OR die(mysqli_error($conn));
+            $numrows = mysqli_num_rows($result);
+            if ($numrows > 0)
+            {
+                echo"<script> alert('This username has been taken!'); window.location='registerUser.php' </script>";
+            }
+    
+            
         
             $sql = "INSERT INTO Users (name, username, email, password)
             VALUES ('".$_POST["Name"]."','".$_POST["Username"]."','".$_POST["Email"]."', '".$_POST["Password"]."')";
--- script.js
+++ script.js
@@ -3,7 +3,4 @@ function slide()
     $(document.getElementById("menuSlide")).slideToggle("slow");
 }
 
-function notLogged()
-{
-    alert("Please log in first!");
-}
+
