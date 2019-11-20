05880950e63234118925a9a737fe502cd40bf9e4
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2019-11-20 22:12:03 +0000

    contact functionality start

---
+++ contactSql.php
@@ -0,0 +1,38 @@
+<html>
+
+<body>
+    <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "project2";
+
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+            echo "Connected successfully <br>";
+    
+            
+        
+            $sql = "INSERT INTO contact (name, email, message)
+            VALUES ('".$_POST["Name"]."','".$_POST["Email"]."', '".$_POST["Message"]."')";
+
+            if ($conn->query($sql) === TRUE)
+            {
+                echo "<br>Message sent!.";
+                header("location: contact.php");
+            }
+            else
+            {
+                echo "Error: <br>".$conn->error;
+            }
+
+            $conn->close();
+        ?>
+</body>
+
+</html>
