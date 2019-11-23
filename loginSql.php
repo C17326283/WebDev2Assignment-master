bf801228a80269c069834029b9421daa0289c870
KyleHeffernan <48320299+KyleHeffernan@users.noreply.github.com>
2019-11-23 18:14:21 +0000

    login now uses ajax

--- loginSql.php
+++ loginSql.php
@@ -2,42 +2,74 @@
 
 <body>
     <?php
-            session_start();
-
+        session_start();
+    
+        if(isset($_POST['submit']))
+        {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            
+            $errorEmpty = false;
+            $errorDetails = false;
+            
             // Create connection
             $conn = new mysqli("localhost", "root", "", "project2");
-
-            // Check connection
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            } 
-            echo "Connected successfully <br>";
-        
-            $userUsername = $_POST["Username"];
-            $userPassword = $_POST["Password"];
-        
-            $sql = "SELECT * FROM users WHERE Username = '$userUsername' AND password ='$userPassword'"; 
-        
+            
+            $sql = "SELECT * FROM users WHERE username = '$username' AND password ='$password'";
             $result = mysqli_query($conn, $sql) OR die(mysqli_error($conn));
-        
             $numrows = mysqli_num_rows($result);
-
-            if ($numrows == 1)
+            
+            
+            if(empty($username) || empty($password))
             {
-                echo "Logged in";
+                echo "<span class='form-error'>Fill in all fields!</span>";
+                $errorEmpty = true;
+            }
+            elseif(!$numrows > 0)
+            {
+                echo "<span class='form-error'>Invalid login details!</span>";
+                $errorDetails = true;
+            }
+            else
+            {
                 $_SESSION['loggedin'] = true;
                 
                 $row = $result->fetch_assoc();
                 $_SESSION['Username'] = $row["username"];//Sets the session username to the username of the account that just logged in
-                header("location: index.php");
+                echo "<span class='form-success'>Logged in successfully!</span>";
+                
+                $conn->close();
             }
-            else
-            {
-                echo "<script type='text/javascript'>alert('Incorrect login details')</script>";
-                echo '<script>window.location.href = "index.php";</script>';
-            }
-            $conn->close();
-        ?>
+        }
+        else
+        {
+            echo "There was an error!";
+        }
+    ?>
+    
+    <script>
+        $("#log-username, #log-password").removeClass("input-error");
+        
+        var errorEmpty = "<?php echo $errorEmpty; ?>";
+        var errorDetails = "<?php echo $errorDetails; ?>";
+
+        
+        if(errorEmpty == true)
+        {
+           $("#log-username, #log-password").addClass("input-error");
+        }
+        if(errorDetails == true)
+        {
+            $("#log-email, #log-password").addClass("input-error");
+        }
+        if(errorEmpty == false && errorDetails == false)
+        {
+            $("#log-username, #log-password, #form-succes").val("");
+            window.location.href = "index.php";
+        }
+        
+    </script>
+
 </body>
 
 </html>
